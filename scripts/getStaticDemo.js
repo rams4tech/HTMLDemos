56347d8b254415b05f677c230c00cc538093754a
rams4tech <rams4tech@gmail.com>
2019-03-01 21:52:41 +0530

    test pull request (#3)
    * latest files for html are added to the repo
    
    * latest files for html are added to the repo
    
    * latest files for html are added to the repo
    
    * latest files for html are added to the repo
    
    * latest files for html are added to the repo
    
    * added new file 2
    
    * Added new files (#4)
    
    * Added new files
    
    * Added bottom as well

--- scripts/getStaticDemo.js
+++ scripts/getStaticDemo.js
@@ -76,6 +76,37 @@ function getServerData(id)
     alert("executed the ajax call");
 }
 
+
+function getServerDataById()
+{
+    var data = document.getElementById('test').value;
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            alert("succesfully executed the ajax calls");
+            document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+        } else {
+
+            document.getElementById("myDiv").innerHTML="<h1>Failed to connect to the server</h1>";
+        }
+    }
+    xmlhttp.open("GET","http://localhost:9090/SystemInfo/rest/hello",true);
+    alert("Sending the ajax call");
+    xmlhttp.send();
+    alert("executed the ajax call");
+}
+
 function postToServer()
 {
     var data = document.getElementById("data").value;
@@ -94,6 +125,9 @@ function postToServer()
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
             document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+        }else {
+
+            document.getElementById("myDiv").innerHTML="<h1>Failed to connect to the server</h1>";
         }
     }
     xmlhttp.open("POST","http://localhost:9090/SystemInfo/rest/hello/post",true);
@@ -122,11 +156,11 @@ function loadTable()
             var resultJSON = JSON.parse(xmlhttp.responseText);
             var html = '';
             for(var i = 0; i < resultJSON.person.length; i++) {
-                html += '<tr><td>' + resultJSON.person[i].firstName + '</td><td>'+
+                 html+= '<tr><td>' + resultJSON.person[i].firstName + '</td><td>'+
                 resultJSON.person[i].lastName + '</td><td>'
                 + resultJSON.person[i].age + '</td></tr>';
             }
-
+            console.log(html);
             document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
             document.getElementById("personTable").innerHTML+=html;
         }
